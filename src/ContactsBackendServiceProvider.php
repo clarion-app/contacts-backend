93c3464614104686654ffeaeac1ad7f469f6a7f1
Tim Schwartz <tim@metaverse.systems>
2025-01-03 09:52:31 -0600

    Updated service provider

--- src/ContactsBackendServiceProvider.php
+++ src/ContactsBackendServiceProvider.php
@@ -2,12 +2,14 @@
 
 namespace ClarionApp\ContactsBackend;
 
-use Illuminate\Support\ServiceProvider;
+use ClarionApp\Backend\ClarionPackageServiceProvider;
 
-class ContactsBackendServiceProvider extends ServiceProvider
+class ContactsBackendServiceProvider extends ClarionPackageServiceProvider
 {
-    public function boot()
+    public function boot(): void
     {
+        parent::boot();
+
         $this->loadRoutesFrom(__DIR__.'/../routes/api.php');
         $this->loadMigrationsFrom(__DIR__.'/../database/migrations');
     }
